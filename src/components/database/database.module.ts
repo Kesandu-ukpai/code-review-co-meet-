edbad7519258e68618459850ab2f96443d53f5ee
Kesandu123 <ukpai.kesandu@gmail.com>
2026-01-22 23:15:21 +0100

    added redis module

--- src/components/database/database.module.ts
+++ src/components/database/database.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         type: 'postgres',
         host: config.get<string>('DB_HOST'),
         port: config.get<number>('DB_PORT'),
-        username:config.get<string>('DB_USERNAME'),
+        username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         autoLoadEntities: true, //find db tables auto
--- src/components/redis/redis.module.ts
+++ src/components/redis/redis.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-ioredis-yet';
+import { CacheModule } from '@nestjs/cache-manager';
 
-@Module({})
-export class RedisModule {}
+
+
+@Module({
+    imports: [
+        //load env from config
+        ConfigModule.forRoot({ isGlobal: true }),
+
+        //connect to redis
+        CacheModule.registerAsync({
+            isGlobal: true,
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                store: await redisStore({
+                    host: config.get<string>('REDIS_HOST'),
+                    port: config.get<number>('REDIS_PORT'),
+                    ttl: 600, // 10 minutes
+                })
+            })
+        })
+    ]
+})
+export class RedisModule { }
